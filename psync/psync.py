b021ca9b8e1d80db0bf004a0e1a1c967a97cac2d
Héricles Emanuel <hericles.me@gmail.com>
2018-10-27 13:30:12 -0300

    add staging entry & index read

--- psync/psync.py
+++ psync/psync.py
@@ -13,6 +13,11 @@ config = {
   "serviceAccount": "./serviceAccountKey.json"
 }
 
+StagingEntry = collections.namedtuple('StagingEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode',
+    'uid', 'gid', 'size', 'sha1', 'flags', 'path',
+])
+
 firebase = pyrebase.initialize_app(config)
 
 auth = firebase.auth()
@@ -42,3 +47,30 @@ def hash_object(data, obj_type, write=True):
             os.makedirs(os.path.dirname(path), exist_ok=True)
             write_file(path, zlib.compress(full_data))
     return sha1
+
+def read_index():
+    try:
+        data = read_file(os.path.join('.git', 'index'))
+    except FileNotFoundError:
+        return []
+    digest = hashlib.sha1(data[:-20]).digest()
+    assert digest == data[-20:], 'invalid index checksum'
+    signature, version, num_entries = struct.unpack('!4sLL', data[:12])
+    assert signature == b'DIRC', \
+            'invalid index signature {}'.format(signature)
+    assert version == 2, 'unknown index version {}'.format(version)
+    entry_data = data[12:-20]
+    entries = []
+    i = 0
+    while i + 62 < len(entry_data):
+        fields_end = i + 62
+        fields = struct.unpack('!LLLLLLLLLL20sH',
+                               entry_data[i:fields_end])
+        path_end = entry_data.index(b'\x00', fields_end)
+        path = entry_data[fields_end:path_end]
+        entry = StagingEntry(*(fields + (path.decode(),)))
+        entries.append(entry)
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+    assert len(entries) == num_entries
+    return entries
