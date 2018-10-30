7db5053e6ff51d4baf9510c3847e3f749e267fa5
Héricles Emanuel <hericles.me@gmail.com>
2018-10-30 09:31:14 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+# -*- coding: utf -*-
+
+setup(
+    name='psync',
+    version='1.0.0',
+    description='CLI para sincronização automática de arquivos e diretórios - Computação@UFCG',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    author='Héricles Emanuel',
+    author_email='hericles.me@gmail.com',
+    url='https://github.com/P-Sync/P-Sync',
+    packages=['psync'],
+    install_requires=['hashlib', 'zlib', 'collections', 'struct', 'pyrebase', 'argparse']
+)
